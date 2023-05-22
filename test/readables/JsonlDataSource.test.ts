151afb27f473d71ea7f7f11e7e33c26d295c4175
devleandrodias <leandrodbdias@gmail.com>
2023-05-22 14:01:07 -0300

    feat: increment coverage from CsvTransform

--- test/readables/JsonlDataSource.test.ts
+++ test/readables/JsonlDataSource.test.ts
@@ -1,15 +1,46 @@
 import "reflect-metadata";
 
+import { DataSourceInput } from "../../src/types/DataSourceInput";
 import { JsonlDataSource } from "../../src/app/readers/JsonlDatasource";
+import { JsonlDataSourceInput } from "../../src/types/JsonlDatasourceInput";
 
 describe("[JsonlDataSource]", () => {
   let jsonlDataSource: JsonlDataSource;
 
+  let dataChunks: DataSourceInput[];
+  let expectedData: DataSourceInput[];
+
   beforeAll(() => {
     jsonlDataSource = new JsonlDataSource();
+
+    expectedData = [
+      {
+        ip: "59.90.255.63",
+        timestamp: 1684196387094,
+        clientId: "1a301e29-6d6f-5e47-b130-e8fb5c0b1ee2",
+      },
+    ];
+
+    dataChunks = [];
   });
 
   it("should be defined", () => {
     expect(jsonlDataSource).toBeDefined();
+  });
+
+  it.skip("should read data from jsonl file", () => {
+    jsonlDataSource.on("data", (row: JsonlDataSourceInput) => {
+      dataChunks.push({
+        ip: row.ip,
+        clientId: row.id,
+        timestamp: row.timestamp,
+      });
+    });
+
+    jsonlDataSource.on("end", () => {
+      expect(dataChunks).toEqual(expectedData);
+    });
+
+    jsonlDataSource._read();
   });
 });
--- test/transfoms/CsvTransform.test.ts
+++ test/transfoms/CsvTransform.test.ts
@@ -1,16 +1,69 @@
 import "reflect-metadata";
 
-import { RedisService } from "../../src/services/RedisService";
+import { IRedisService } from "../../src/services/RedisService";
+import { DataSourceInput } from "../../src/types/DataSourceInput";
 import { CsvTransform } from "../../src/app/transforms/CsvTransform";
+import { GeolocationOutput } from "../../src/types/GeolocationOutput";
+import { RedisInMemoryService } from "../services/RedisInMemoryService";
 
 describe("[CsvTransform]", () => {
+  let redisService: IRedisService;
   let csvTransform: CsvTransform;
 
+  let getLocationSpy: jest.SpyInstance;
+  let setLocationSpy: jest.SpyInstance;
+
   beforeAll(() => {
-    csvTransform = new CsvTransform(new RedisService());
+    redisService = new RedisInMemoryService();
+    csvTransform = new CsvTransform(redisService);
+
+    getLocationSpy = jest.spyOn(RedisInMemoryService.prototype, "getLocation");
+    setLocationSpy = jest.spyOn(RedisInMemoryService.prototype, "setLocation");
   });
 
   it("should be defined", () => {
     expect(csvTransform).toBeDefined();
+  });
+
+  describe("scenarios with geolocation already exists in cache", () => {
+    const ip = "30.46.245.122";
+    const clientId = "95cdb0f2-9487-5bfd-aeda-bac27dd406fa";
+    const timestamp = new Date().getTime();
+
+    const chunk: DataSourceInput = {
+      ip,
+      clientId,
+      timestamp,
+    };
+
+    const geolotionAlreadyInCache: GeolocationOutput = {
+      ip,
+      clientId,
+      timestamp,
+      city: "Columbus",
+      region: "Ohio",
+      country: "United States",
+      latitude: 39.97883,
+      longitude: -82.89573,
+    };
+
+    beforeAll(async () => {
+      await redisService.setLocation(geolotionAlreadyInCache);
+    });
+
+    afterAll(async () => {
+      await redisService.delLocation(ip);
+    });
+
+    it("should return null if geolocation already exists in cache", async () => {
+      await csvTransform._transform(chunk, "utf-8", (error) => {
+        if (error) {
+          console.error(error);
+        }
+      });
+
+      expect(getLocationSpy).toHaveBeenCalledWith(chunk.ip);
+      expect(setLocationSpy).not.toHaveBeenCalled();
+    });
   });
 });
