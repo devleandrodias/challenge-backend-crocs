e3b9525693d96b22bb899edca3786c07e20b62c2
devleandrodias <leandrodbdias@gmail.com>
2023-05-22 20:08:35 -0300

    feat: updating redis to use current timestamp

--- src/services/RedisService.ts
+++ src/services/RedisService.ts
@@ -2,6 +2,7 @@ import { injectable } from "tsyringe";
 import { RedisClientType, createClient } from "redis";
 
 import { envs } from "../configs/env.config";
+import { loggerInfo } from "../utils/logger";
 import { parseStringToObj } from "../utils/parser";
 import { GeolocationOutput } from "../types/GeolocationOutput";
 
@@ -10,6 +11,8 @@ export interface IRedisService {
   getLocation(ip: string): Promise<GeolocationOutput | null>;
   delLocation(ip: string): Promise<void>;
 }
+
+const MAX_TIME_IN_CACHE_SECONDS: number = 60 * 30;
 
 @injectable()
 export class RedisService implements IRedisService {
@@ -24,8 +27,22 @@ export class RedisService implements IRedisService {
   }
 
   async setLocation(location: GeolocationOutput): Promise<void> {
+    const diff = new Date().getTimezoneOffset();
+
+    const currentTime = new Date().getTime() - diff;
+
+    if (location.timestamp <= currentTime) {
+      loggerInfo({
+        type: "warning",
+        log: "The provided timestamp has already passed. The record will not be saved to Redis.",
+      });
+      return;
+    }
+
+    const expirationTimestamp = location.timestamp + MAX_TIME_IN_CACHE_SECONDS;
+
     await this.client.set(location.ip, JSON.stringify(location), {
-      EX: 60,
+      EX: expirationTimestamp,
       NX: true,
     });
   }
